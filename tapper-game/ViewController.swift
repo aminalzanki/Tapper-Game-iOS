05dfdace205676aad0cedab5a1f6b2650d3c28fb
Nik Muhammad Amin <nik.muhammad.amin@gmail.com>
2016-04-03 15:05:30 +0800

    Initial Commit

---
+++ tapper-game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper-game
+//
+//  Created by Nik Muhammad Amin on 4/3/16.
+//  Copyright © 2016 aminalzanki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
